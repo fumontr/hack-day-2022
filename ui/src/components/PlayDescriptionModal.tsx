3a38482147697ac8336a72d06543608e4fd8e88a
furiko <furiko522code@gmail.com>
2022-09-18 05:56:19 +0900

    update how to modal

--- ui/src/components/PlayDescriptionModal.tsx
+++ ui/src/components/PlayDescriptionModal.tsx
@@ -7,6 +7,11 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  OrderedList,
+  UnorderedList,
+  ListItem,
+  Text,
+  Flex
 } from "@chakra-ui/react";
 
 type PlayDescriptionModalProps = {
@@ -19,38 +24,48 @@ export const PlayDescriptionModal: React.FC<PlayDescriptionModalProps> = ({
   onHowToClose,
 }) => {
   return (
-    <Modal isOpen={howToOpen} onClose={onHowToClose}>
+    <Modal isOpen={howToOpen} onClose={onHowToClose} size="lg" isCentered>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>遊びかた？</ModalHeader>
+        <ModalHeader justifyContent="center">
+          <Flex justifyContent="center">
+            あそびかた？
+          </Flex>
+        </ModalHeader>
         <ModalCloseButton />
-        <ModalBody>ああ</ModalBody>
-        <ModalFooter>
+        <ModalBody>
+          <Text fontSize="xl" fontWeight="bold">あそびかた</Text>
+          <OrderedList>
+            <ListItem>ビデオカメラをONにしてください。</ListItem>
+            <ListItem>あなたがうごいたほうこうにシーソーもうごきます。</ListItem>
+            <ListItem>あなたがうごいたほうこうにシーソーもうごきます。</ListItem>
+          </OrderedList>
+          <Text fontSize="xl" fontWeight="bold" pt="8px">クリアするには？</Text>
+          <UnorderedList>
+            <ListItem>
+              <Text fontWeight="bold">
+                ひとりであそぶ。
+              </Text>
+            </ListItem>
+            <Text>
+              ゲームがスタートすると、ひだりうえからボールがでます。ボールをゴールにいれるとクリアです。
+            </Text>
+            <ListItem>
+              <Text fontWeight="bold">
+                みんなであそぶとは？
+              </Text>
+            </ListItem>
+            <Text>
+              ふたりできょうりょくしてゴールまでボールをはこんでください。
+            </Text>
+          </UnorderedList>
+        </ModalBody>
+        <ModalFooter justifyContent="center">
           <Button colorScheme="blue" mr={3} onClick={onHowToClose}>
-            Close
+            さっそくあそぶ！
           </Button>
         </ModalFooter>
       </ModalContent>
     </Modal>
-    // <div className="overflow-y-auto overflow-x-hidden fixed z-50 p-16 h-full w-full top-0 right-0 left-0 drop-shadow-2xl">
-    //   <div className="bg-white h-full p-20">
-    //     <div className="p-6 space-y-6">
-    //       <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-    //         「ひとりであそぶ」
-    //       </p>
-    //       <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-    //         「ふたりであそぶ」
-    //       </p>
-    //     </div>
-    //     <div className="flex justify-between">
-    //       <button
-    //         onClick={close}
-    //         className="flex-1 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-32"
-    //       >
-    //         とじる
-    //       </button>
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
