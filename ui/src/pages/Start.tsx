37f7029014301a422455568171037be7e9c75eaf
furiko <furiko522code@gmail.com>
2022-09-18 04:51:32 +0900

    update top page

--- ui/src/pages/Start.tsx
+++ ui/src/pages/Start.tsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import { PlayDescriptionMordal } from "../components/PlayDescriptionMordal"
 import { RoomCreateMordal } from "../components/RoomCreateMordal"
 
+import { Flex, HStack, Button, Text, Stack } from '@chakra-ui/react'
+import BackgroundImage from '../assets/background.png'
+
 export const Start = ({setMyId, setFriendId, setRoomId}) => {
   const [dialogDisplayState, setDialogDisplayState] = useState(false)
   const [showPlayDescription, setShowPlayDescription] = useState(false)
@@ -17,28 +20,34 @@ export const Start = ({setMyId, setFriendId, setRoomId}) => {
     setDialogDisplayState(true)
   }
 
-  return (
+return (
     <>
-    {showPlayDescription && <PlayDescriptionMordal setDialogDisplayState = {setDialogDisplayState} setShowPlayDescription={setShowPlayDescription} />}
-    {roomCreateMordalDisplayState && <RoomCreateMordal setRoomId={setRoomId} setFriendId = {setFriendId} setMyId={setMyId} setDialogDisplayState = {setDialogDisplayState} setRoomCreateMordalDisplayState={setRoomCreateMordalDisplayState}/> }
-     <div className="flex p-20 h-screen bg-white">
-      <div className="flex-1" >
-        プレイ画像サンプル領域
-       <button className="bg-gray-300 hover:bg-blue-700 text-black-900 font-bold py-2 px-4 rounded w-full">ひとりであそぶ</button>
-       <button onClick={onClickRoomCreate} className="bg-gray-300 hover:bg-blue-700 text-black-900 font-bold py-2 px-4 rounded w-full">みんなであそぶ</button>
-       {/*<ul className="flex-1 flex flex-col justify-between py-20">*/}
-        {/*<li className="text-3xl p-8 text-center">*/}
-        {/*  <button onClick={onClickPlayDescription} className="bg-gray-300 hover:bg-blue-700 text-black-900 font-bold py-2 px-4 rounded w-full">あそびかた</button>*/}
-        {/*</li>*/}
-        {/*<li className="text-3xl p-8 text-center">*/}
-        {/*  <button className="bg-gray-300 hover:bg-blue-700 text-black-900 font-bold py-2 px-4 rounded w-full">ひとりであそぶ</button>*/}
-        {/*</li>*/}
-        {/*<li className="text-3xl p-8 text-center">*/}
-        {/*  <button onClick={onClickRoomCreate} className="bg-gray-300 hover:bg-blue-700 text-black-900 font-bold py-2 px-4 rounded w-full">みんなであそぶ</button>*/}
-        {/*</li>*/}
-      {/*</ul>*/}
-     </div>
-    </div>
+      {showPlayDescription && <PlayDescriptionMordal setDialogDisplayState = {setDialogDisplayState} setShowPlayDescription={setShowPlayDescription} />}
+      {roomCreateMordalDisplayState && <RoomCreateMordal setRoomId={setRoomId} setFriendId = {setFriendId} setMyId={setMyId} setDialogDisplayState = {setDialogDisplayState} setRoomCreateMordalDisplayState={setRoomCreateMordalDisplayState}/> }
+       <Flex className="flex p-20 h-screen bg-white" backgroundImage={BackgroundImage} backgroundPosition="center" backgroundSize="70%">
+         <Stack w="full">
+           <Flex justifyContent="right" transform="rotate(0.1)">
+             <Text fontSize="40px" fontWeight="bold" color="#86E2FF" borderBottom="4px" background="white">シーソーオンライン</Text>
+           </Flex>
+          <HStack w="full" h="full">
+            <Button h="400px" className="bg-gray-300 hover:bg-blue-700 text-black-900 font-bold py-2 px-4 rounded w-full" opacity="0.9">
+              <Text fontSize="4xl">
+                ひとりであそぶ
+              </Text>
+            </Button>
+            <Button  h="400px" onClick={onClickRoomCreate} className="bg-gray-300 hover:bg-blue-700 text-black-900 font-bold py-2 px-4 rounded w-full" opacity="0.9">
+              <Text fontSize="4xl">
+                みんなであそぶ
+              </Text>
+            </Button>
+          </HStack>
+           <Flex justifyContent="right">
+             <Button w="80px" h="80px" justifyContent="center" borderRadius="40px" onClick={onClickPlayDescription}>
+               <Text fontSize="2xl">?</Text>
+             </Button>
+           </Flex>
+         </Stack>
+      </Flex>
     </>
   )
 };
