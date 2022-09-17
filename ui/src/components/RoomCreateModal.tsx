ec82e59597c91e7b876d64f3a6c870f593333d1e
Takahiro Nishino <bigstitch99@gmail.com>
2022-09-18 05:11:36 +0900

    playに遷移

--- ui/src/components/RoomCreateModal.tsx
+++ ui/src/components/RoomCreateModal.tsx
@@ -34,8 +34,12 @@ export const RoomCreateModal: React.FC<RoomCreateModalProps> = ({
     method: "POST",
     headers: { "Content-Type": "application/json" },
   };
-  const inputRef = useRef<HTMLInputElement>();
+
   const close = async () => {
+    if (val.length !== 3) return alert("あいことばは３文字で入力してね！");
+    // hiragana regex
+    if (!/^[\u3040-\u309F]+$/.test(val))
+      return alert("あいことばは、ひらがなだよ！");
     return fetch(
       `https://backend-dot-hack-day-2022-362804.de.r.appspot.com/rooms/${val}/join`,
       requestOptions
@@ -57,7 +61,12 @@ export const RoomCreateModal: React.FC<RoomCreateModalProps> = ({
         <ModalHeader>ひらがな３文字の あいことば を入力</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <Input size={"lg"} placeholder="あいことば" />
+          <Input
+            value={val}
+            size={"lg"}
+            placeholder="あいことば"
+            onChange={(e) => setVal(e.target.value)}
+          />
         </ModalBody>
         <ModalFooter>
           <Button colorScheme="blue" mr={3} onClick={close}>
