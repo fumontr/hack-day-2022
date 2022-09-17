b76d8220b21da21153d498bab989250dfdd2cd10
furiko <furiko522code@gmail.com>
2022-09-17 19:13:29 +0900

    :sparkles: add websocket broadcast function

--- server/handler/ws.go
+++ server/handler/ws.go
@@ -1,16 +1,19 @@
 package handler
 
 import (
+	"furiko/hack-day-2022/model"
 	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
 	"log"
 )
 
 var (
-	upgrader = websocket.Upgrader{}
+	upgrader  = websocket.Upgrader{}
+	broadcast = make(chan model.Movement)
+	clients   = make(map[*websocket.Conn]bool)
 )
 
-func Ws(c echo.Context) error {
+func WebsocketSample(c echo.Context) error {
 	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
 	if err != nil {
 		log.Print("upgrade:", err)
@@ -31,4 +34,42 @@ func Ws(c echo.Context) error {
 		}
 	}
 	return nil
+}
+
+func ConnectWebsocket(c echo.Context) error {
+	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return nil
+	}
+	defer ws.Close()
+
+	clients[ws] = true
+
+	for {
+		var movement model.Movement
+		err = ws.ReadJSON(&movement)
+		if err != nil {
+			log.Println("connection closed from client:", err)
+			break
+		}
+		broadcast <- movement
+	}
+	return nil
+}
+
+func HandleMessage() {
+	for {
+		// broadcastチャネルからメッセージを受け取る
+		message := <-broadcast
+		// 接続中の全クライアントにメッセージを送る
+		for client := range clients {
+			err := client.WriteJSON(message)
+			if err != nil {
+				log.Printf("error: %v", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
 }
